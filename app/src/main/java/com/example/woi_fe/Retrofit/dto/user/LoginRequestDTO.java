e5257db1cc0bc3feefd45c6606d616ce2108cd37
kbannie <kabin2205@gmail.com>
2024-06-14 10:45:57 +0900

    [Feat] 로그인 기능 구현

--- app/src/main/java/com/example/woi_fe/Retrofit/dto/user/LoginRequestDTO.java
+++ app/src/main/java/com/example/woi_fe/Retrofit/dto/user/LoginRequestDTO.java
@@ -3,4 +3,20 @@ package com.example.woi_fe.Retrofit.dto.user;
 public class LoginRequestDTO {
     private String loginId;
     private String password;
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
